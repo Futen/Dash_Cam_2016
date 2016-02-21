0643194a8d8d8cfe88ec1d36ac957ef8d625d2e2
Futen <tdk356@yahoo.com.tw>
2016-02-21 20:58:54 +0800

    gg

---
+++ DownloadPano/old/RunDownloadList_NegSource.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+
+import sys
+sys.path.append('/home/Futen/Dash_Cam_2016')
+import SystemParameter as SP
+import GetCircleBound
+import PanoProcess
+import subprocess
+from multiprocessing import Pool
+import numpy as np
+import time
+
+number_to_do = 247
+number = 0
+
+def DownloadList(center):   # average 42 second for one video
+    pano_lst = []
+    err_lst = []
+    bound = GetCircleBound.GetCircleBound(center)
+    for latlon in bound:
+        try:
+            pano_name = PanoProcess.GetPanoID(latlon)
+        except:
+            print 'error %f, %f'%(latlon[0],latlon[1])
+            err_lst.append((latlon[0], latlon[1]))
+            continue
+        if pano_name != None:
+            pano_lst.append((pano_name, latlon[0], latlon[1]))
+    return (pano_lst, err_lst)
+def Download(video_info): # (vname, lat, lon))
+    global number
+    #if number >= number_to_do:
+    #    return
+    print '# %d'%number,video_info
+    vname = video_info[0]
+    latlon = (float(video_info[1]), float(video_info[2]))
+    path =  SP.GetNegSourcePath(vname)# get all path
+    #print path
+    
+    if path['state']['panolist'] == 'no':
+        #print vname,latlon
+        result = DownloadList(latlon)
+        pano_lst = result[0]
+        err_lst = result[1]
+        
+        f = open('%s/pano_lst.txt'%path['pano_path'], 'w')
+        for pano in pano_lst:
+            #print pano
+            s = pano[0] + '\t' + str(pano[1]) + '\t' + str(pano[2]) + '\n'
+            f.write(s)
+        f.close()
+        f = open('%s/pano_error_lst.txt'%path['pano_path'], 'w')
+        for pano in err_lst:
+            #print pano
+            s = str(pano[0]) + '\t' + str(pano[1]) + '\n'
+            f.write(s)
+        f.close()
+        command = 'mv %s/pano_lst.txt %s/pano_lst_finish.txt'%(path['pano_path'], path['pano_path'])
+        subprocess.call(command, shell=True)
+        number += 1
+    else:
+        print '%s finish'%vname
+    
+if __name__ == '__main__':
+    pool = Pool(processes = 1)
+    lst = SP.GetNegSourceList(LATLON = True)
+    #Download(lst[-1])
+    pool.map(Download, lst)
+
+
+
+
+
+
---
+++ DownloadPano/old/RunDownloadPano_NegSource.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+import sys
+sys.path.append('/home/Futen/Dash_Cam_2016')
+import SystemParameter as SP
+import GetCircleBound
+import PanoProcess
+import subprocess
+import numpy as np
+import os
+from multiprocessing import Pool
+import GetPanoByID
+import time
+
+
+def Download(v_name):
+    start = time.time()
+    info = SP.GetNegSourcePath(v_name)
+    if info['state']['panolist'] == 'no':
+        return 
+    print info
+    if os.path.isfile('%s/pano_lst_precise.txt'%info['pano_path']):
+        return
+    #subprocess.call('rm %s/*'%info['pano_uncut_path'], shell=True)
+    #subprocess.call('rm %s/*'%info['pano_cut_path'],shell=True)
+    pano_file = open(info['pano_path']+'/pano_lst_finish.txt','r')
+    pano_out_file = open(info['pano_path']+'/pano_lst_tmp.txt','w')
+    finish_lst = []
+    for line in pano_file:
+        line = line.split('\t')
+        ID = line[0]
+        location = PanoProcess.GoogleSV.getLocationbyID(ID)
+        #print location
+        if ID in finish_lst:
+            continue
+        check = GetPanoByID.GetPanoByID(ID, info['pano_cut_path'])
+        if check == True:
+            s = 'pano_' + ID + '.jpg' + '\t' + location[0] + '\t' + location[1] + '\n'
+            pano_out_file.write(s)
+            finish_lst.append(ID)
+        else:
+            print 'error %s'%ID
+            error_file = open('%s/download_error_lst.txt'%info['pano_path'],'a')
+            #subprocess.call('rm -r */', shell=True)
+            if location != None:
+                error_file.write('%s\t%s\t%s\n'%(ID,location[0], location[1]))
+            error_file.close()
+            continue
+        time.sleep(1)
+        
+    pano_file.close()
+    pano_out_file.close()
+    subprocess.call('mv %s %s'%(info['pano_path']+'/pano_lst_tmp.txt', info['pano_path']+'/pano_lst_precise.txt'),shell=True)
+
+    pano_cut_lst = open(info['pano_path'] + '/pano_cut_lst.txt','w')
+    cut_lst = os.listdir(info['pano_cut_path'])
+    cut_lst.sort()
+    for img in cut_lst:
+        img = img + '\n'
+        pano_cut_lst.write(img)
+    pano_cut_lst.close()
+    total_time = time.time() - start
+    print '%s Consume %f minutes'%(v_name, total_time/60)
+    return 
+
+if __name__ == '__main__':
+    pool = Pool(processes = 1)
+    lst = SP.GetNegSourceList()
+    do_lst = []
+    for one in lst:
+        info = SP.GetNegSourcePath(one)
+        if info['state']['panodownload'] == 'no':
+            do_lst.append(one)
+    #Download(lst[-1])
+    pool.map(Download, do_lst)
