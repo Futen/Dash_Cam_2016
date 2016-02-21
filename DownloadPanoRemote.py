d849dad964724257f9b1481b67608132fba0c1ad
Futen <tdk356@yahoo.com.tw>
2016-02-21 21:44:00 +0800

    gg

--- DownloadPanoRemote.py
+++ DownloadPanoRemote.py
@@ -17,6 +17,24 @@ def GetList(f_name):
         lst.append(line[0:-1])
     f.close()
     return lst
+def Download(v_name_comprass):
+    v_name = v_name_comprass[0]
+    seg_type = v_name_comprass[1]
+    info = SP.GetPath(v_name, seg_type)
+    subprocess.call('mkdir -p %s'%info['pano_cut_path'], shell=True)
+    subprocess.call('mkdir -p %s'%info['pano_uncut_path'], shell=True)
+    command = 'sshpass -p pig6983152 scp Faraday:%s/pano_lst_finish.txt %s'%(info['pano_path'], info['pano_path'])
+    subprocess.call(command, shell=True)
+    libRundownload.DownloadPano(v_name_comprass)
+    command = 'sshpass -p pig6983152 scp %s/pano_lst_precise.txt %s/pano_cut_lst.txt Faraday:%s'%(
+                info['pano_path'],
+                info['pano_path'],
+                info['pano_path'],
+    #subprocess.call(command, shell=True)
+    print command
+    command = 'sshpass -p pig6983152 scp %s/* Faraday:%s'%(info['pano_cut_path'], info['pano_cut_path'])
+    #subprocess.call(command, shell=True)
+    print command
 
 if __name__ == '__main__':
     argv = sys.argv
@@ -25,5 +43,6 @@ if __name__ == '__main__':
         exit()
     lst = GetList(argv[1])
     lst = libRundownload.ArgumentComprass(lst, TYPE)
-    pool = Pool(processes = 1)
-    pool.map(libRundownload.DownloadPano, lst)
+    tmp = ()
+    #pool = Pool(processes = 1)
+    #pool.map(Download, lst)
