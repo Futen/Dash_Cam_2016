6fdc22640207a124ca659528a72a687ae712351b
Futen <tdk356@yahoo.com.tw>
2016-02-22 17:03:20 +0800

    gg

--- DownloadPanoRemote.py
+++ DownloadPanoRemote.py
@@ -5,7 +5,7 @@ sys.path.append('/home/Futen/Dash_Cam_2016/')
 sys.path.append('/home/Futen/Dash_Cam_2016/DownloadPano')
 import os
 import subprocess
-import SystemParameter as SP
+import SystemParameter_For_Download as SP
 import libRundownload
 from multiprocessing import Pool
 ##
