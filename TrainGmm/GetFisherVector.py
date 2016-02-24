b4174e199a66c1eb816e8a8b087ba477fa3b5334
Futen <tdk356@yahoo.com.tw>
2016-02-24 15:44:54 +0800

    gg

---
+++ TrainGmm/GetFisherVector.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('/home/Futen/Dash_Cam_2016/')
+import ExpList as EL
+from multiprocessing import Pool
+import SendEmail
+
+if __name__ == '__main__':
+    pool = Pool(processes = 12)
+    try:
+    except:
+
